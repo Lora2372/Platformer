5dd421358304a12aebd56bb7b293424fd04c5aab
Lora <Lora.Twinblade@gmail.com>
2015-12-03 10:42:37 +0100

    Version: 0.1.075.1
    Bug fix:
    - Fixed the naming of the animated lever.

--- Lora's Platformer 2.0/src/Main/GamePanel.java
+++ Lora's Platformer 2.0/src/Main/GamePanel.java
@@ -20,7 +20,7 @@ public class GamePanel
 	public static int WIDTH = 1200;
 	public static int HEIGHT = 900;
 	
-	public static final String version = "0.1.075";
+	public static final String version = "0.1.075.1";
 		
 	// Game thread
 	private Thread thread;
